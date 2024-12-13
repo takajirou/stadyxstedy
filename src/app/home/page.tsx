943be6d65c6dbf8e02bea7face062fd1f1a05723
sojiro takagi <soujirou.takagi@gmail.com>
2024-12-13 16:39:37 +0900

    supabase追加

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,6 +1,32 @@
+"use client";
+
 import Objective from "@/components/Objective";
+import { useEffect, useState } from "react";
+import { supabase } from "@/lib/Supabase";
+
+interface GrandObjective {
+    id: number;
+    grandObject: string;
+}
 
 export default function Home() {
+    const [GrandObjective, setGrandObjective] = useState<GrandObjective[]>([]);
+
+    useEffect(() => {
+        const fetchGrandObjective = async () => {
+            const { data, error } = await supabase.from("grandObjective").select("*");
+            if (error) {
+                console.error(error);
+            } else {
+                console.log(data);
+                setGrandObjective(data);
+            }
+        };
+
+        fetchGrandObjective();
+    }, []);
+
+    console.log(GrandObjective);
     return (
         <>
             <footer></footer>
