b53be4d9e47358566775e42948923d1c19ff061f
sojiro takagi <soujirou.takagi@gmail.com>
2024-12-31 22:11:52 +0900

    諸々修正

--- src/lib/Supabase.tsx
+++ src/lib/Supabase.tsx
@@ -1,4 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
+if (!supabaseUrl || !supabaseKey) {
+    throw new Error("Supabase URL or Key is not set in the environment variables");
+}
 export const supabase = createClient(supabaseUrl, supabaseKey);
