880a8972f0fb170443eab8cba36652654a82f764
sojiro takagi <soujirou.takagi@gmail.com>
2025-01-09 09:58:53 +0900

    カウンター追加

--- src/components/DayCounter.tsx
+++ src/components/DayCounter.tsx
@@ -1,9 +1,30 @@
+"use client";
+
 import styles from "@styles/componentStyles/home/DayCounter.module.scss";
+import { useEffect, useState } from "react";
 
 export default function DayCounter() {
+    const [counter, setCounter] = useState({ count: 0 });
+
+    useEffect(() => {
+        const fetchCounter = async () => {
+            const response = await fetch("/DayCounter.json");
+
+            if (!response.ok) {
+                throw new Error(`Error fetching counter.json: ${response.status}`);
+            }
+
+            const data = await response.json();
+
+            setCounter(data);
+        };
+
+        fetchCounter();
+    }, []);
+
     return (
         <div className={styles.DayCounterWrap}>
-            <h1>連続学習中！</h1>
+            <h1>{counter.count}日連続学習中！</h1>
         </div>
     );
 }
