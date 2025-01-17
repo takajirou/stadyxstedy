27ff89561cfbf541211d069b9e3d1cefc6d25215
sojiro takagi <soujirou.takagi@gmail.com>
2025-01-17 19:21:02 +0900

    フォント設定

---
+++ src/fonts.tsx
@@ -0,0 +1,7 @@
+import { Orbitron } from "next/font/google";
+
+export const orbitron = Orbitron({
+    variable: "--font-orbitron",
+    subsets: ["latin"],
+    weight: ["400", "500", "700"],
+});
