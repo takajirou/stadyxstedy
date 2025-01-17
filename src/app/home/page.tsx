19e0d601d3550737b72874e983718639e22e678c
sojiro takagi <soujirou.takagi@gmail.com>
2025-01-17 13:40:45 +0900

    homeにプロフィールボタン追加

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,11 +2,15 @@ import Objective from "@/components/home/Objective";
 import Schedule from "@/components/home/Schedule";
 import styles from "@styles/appStyles/home/Page.module.scss";
 import DayCounter from "@/components/home/DayCounter";
+import { CgProfile } from "react-icons/cg";
 
 export default function Home() {
     return (
         <>
             <div className={styles.PageWrap}>
+                <header className={styles.Header}>
+                    <CgProfile size="30px" color="#333" />
+                </header>
                 <Objective Objective="grand" />
                 <Objective Objective="week" />
                 <Schedule />
