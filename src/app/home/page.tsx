4e2f876e5cc10f6c949769c2ac57c9c33257a9bd
sojiro takagi <soujirou.takagi@gmail.com>
2025-01-17 14:45:48 +0900

    header追加

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,15 +2,13 @@ import Objective from "@/components/home/Objective";
 import Schedule from "@/components/home/Schedule";
 import styles from "@styles/appStyles/home/Page.module.scss";
 import DayCounter from "@/components/home/DayCounter";
-import { CgProfile } from "react-icons/cg";
+import Header from "@/components/Header";
 
 export default function Home() {
     return (
         <>
+            <Header />
             <div className={styles.PageWrap}>
-                <header className={styles.Header}>
-                    <CgProfile size="30px" color="#333" />
-                </header>
                 <Objective Objective="grand" />
                 <Objective Objective="week" />
                 <Schedule />
---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import { CgProfile } from "react-icons/cg";
+import styles from "@styles/componentStyles/Header.module.scss";
+
+export default function Header() {
+    return (
+        <header className={styles.Header}>
+            <button>
+                <CgProfile size="30px" color="#333" />
+            </button>
+            <Image src="/images/logo.svg" alt="logo" width={150} height={75} />
+        </header>
+    );
+}
