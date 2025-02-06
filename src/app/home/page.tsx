63237f8d9200e3ce0bdf8093ce33a4086008fa8e
sojiro takagi <soujirou.takagi@gmail.com>
2025-02-07 00:48:21 +0900

    更新

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,5 +1,5 @@
 import Objective from "@/components/home/Objective";
-import WrapSchedule from "@/components/home/WrapSchedule";
+// import WrapSchedule from "@/components/home/WrapSchedule";
 import styles from "@styles/appStyles/home/Page.module.scss";
 // import DayCounter from "@/components/home/DayCounter";
 import Header from "@/components/Header";
