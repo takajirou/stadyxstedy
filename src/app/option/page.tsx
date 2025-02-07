a299f0c54c97333cb8ca861693daf79f8c35493d
sojiro takagi <soujirou.takagi@gmail.com>
2025-02-07 11:06:14 +0900

    delete追加

---
+++ src/app/option/page.tsx
@@ -0,0 +1,23 @@
+import { supabase } from "@/lib/supabaseClient";
+
+export default function Page() {
+    const today = new Date();
+    const DeleteSchedule = async () => {
+        const { error } = await supabase
+            .from("Schedule")
+            .delete()
+            .eq("date", today.toISOString().split("T")[0])
+            .single();
+
+        if (error) {
+            console.error("Error fetching schedule data:", error);
+        }
+    };
+
+    return (
+        <div>
+            <h1>Option Page</h1>
+            <button onClick={DeleteSchedule}>unfinished</button>
+        </div>
+    );
+}
